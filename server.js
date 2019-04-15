f6a0246e45f16ee37c1c6164ed65f59535864d15
Sami Austin <samicaustin@gmail.com>
2019-04-15 13:45:54 -0600

    Delete functioning

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const humans = require('./models/humans');
+const methodOverride = require('method-override')
 const app = express();
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(methodOverride(`_method`));
 
 // INDEX ROUTE
 app.get('/humans', (req, res) => {
@@ -15,6 +17,13 @@ app.get('/humans', (req, res) => {
 
 // SHOW ROUTE
 app.get('')
+
+// DELETE ROUTE
+app.delete('/humans/:id', (req, res) => {
+    humans.splice(req.params.id, 1);
+    res.redirect('/humans');
+    console.log("delete route hit")
+});
 
 
 app.listen(3000, function(){
